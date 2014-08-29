b06b8828de64b95ed3cfdb15d95e7d5d289b100f
Robmass94 <robmass94@gmail.com>
2014-08-29 19:49:07 -0400

    Added struct for parsing user arguments

--- src/shell.c
+++ src/shell.c
@@ -3,8 +3,15 @@
 #include <string.h>
 #include <unistd.h>
 
-char *trimnewline(char *str);
+typedef struct
+{
+	int argc;
+	char* argv[128];
+} Args;
+
+void handleCommand(Args*);
 void shell();
+Args* getUserInput();
 
 int main(int argc, char* argv[])
 {
@@ -14,34 +21,50 @@ int main(int argc, char* argv[])
 
 void shell()
 {
+	Args* input;
+	while (1)
+	{
+		input = getUserInput();
+		handleCommand(input);
+
+		// clear input
+		while (input->argc != 0)
+		{
+			input->argv[input->argc--] = NULL;
+		}
+
+		free(input);
+	}
+}
+
+Args* getUserInput()
+{
+	Args* args = malloc(sizeof(Args));
 	char response[256];
 	char hostname[256];
 	char working_directory[256];
+	char* tok_response;
+	int i;
+	args->argc = 0;
 	gethostname(hostname, 256);
-	do
+	getcwd(working_directory, 256);
+
+	printf("%s@%s:%s $ ", getlogin(), hostname, working_directory);
+	fgets(response, 256, stdin);
+	response[strlen(response) - 1] = '\0'; // remove '\n' character
+	tok_response = strtok(response, " ");
+	while (tok_response != NULL)
 	{
-		getcwd(working_directory, 256);
-		printf("%s@%s:%s $ ", getlogin(), hostname, working_directory);
-		fgets(response, 256, stdin);
-		trimnewline(response);
-		if (strcmp(response, "exit") == 0)
-		{
-			exit(EXIT_SUCCESS);
-		}
-	} while (1);
+		args->argv[args->argc++] = tok_response;
+		tok_response = strtok(NULL, " ");
+	}
+	return args;
 }
 
-char *trimnewline(char *str)
+void handleCommand(Args* response)
 {
-	int i;
-	for(i = 0; i < strlen(str); i++)
+	if (strcmp(response->argv[0], "exit") == 0)
 	{
-
-		if (str[i] == '\n')
-		{
-
-			str[i] = '\0';
-		}
+		exit(EXIT_SUCCESS);
 	}
-	return str;
 }
